2f75af037de02fd54214abeb2a9e3f1bd20fad33
Dridi19 <97188701+Dridi19@users.noreply.github.com>
2022-04-19 01:21:02 +0100

    Delete docs directory

--- docs/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-$engine = "mysql";
-$host = "localhost";
-$port = 3306;
-$dbname = "sakila";
-$username = "root";
-$password = "root";
-$pdo = new PDO("$engine:host=$host:$port;dbname=$dbname", $username, $password);?>
-<p>Test</p>
