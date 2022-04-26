644adc90b642dca7d79d1dce4e42e601e0e5dbf9
Dridi19 <dridimohamed19@gmail.com>
2022-04-26 03:33:22 +0200

    FFFFFFF

--- js/Person.js
+++ js/Person.js
@@ -6,10 +6,10 @@ class Person extends GameObject {
     this.isPlayerControlled = config.isPlayerControlled || false;
 
     this.directionUpdate = {
-      "up": ["y", -2],
-      "down": ["y", 2],
-      "left": ["x", -2],
-      "right": ["x", 2],
+      "up": ["y", -1],
+      "down": ["y", 1],
+      "left": ["x", -1],
+      "right": ["x", 1],
     }
   }
 
@@ -62,7 +62,7 @@ class Person extends GameObject {
   updatePosition() {
       const [property, change] = this.directionUpdate[this.direction];
       this[property] += change;
-      this.movingProgressRemaining -= 2;
+      this.movingProgressRemaining -= 1;
 
       if (this.movingProgressRemaining === 0) {
         utils.emitEvent("PersonWalkingComplete", {
